8e4eb11b0e2b9d422e0d672eb020a911815cbc44
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-05-12 23:55:28 +0530

    ok it's literally ditto now

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require("openai");
+const path = require('path');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,12 +19,17 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/quickstart/:prompt', async (req, res) => {
   const prompt = req.params.prompt;
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Generate a title and 6 creative lines with the prompt ${prompt}. Format result as HTML.`,
+      prompt: `Write a title and 6 lines for the prompt ${nameToExpand}. Format the result as HTML.`,
       temperature: 0.7,
       max_tokens: 256,
       top_p: 1,
@@ -28,15 +37,37 @@ app.get('/quickstart/:prompt', async (req, res) => {
       presence_penalty: 0,
     });
 
-    const qsresult = response.data.choices[0].text;
-    res.json(qsresult);
+    const acros = response.data.choices[0].text;
+    res.json(acros);
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while processing your request.');
   }
 });
 
+// app.get('/quickstart/:prompt', async (req, res) => {
+//   const prompt = req.params.prompt;
+//   try {
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003",
+//       prompt: `Generate a title and 4 creative lines with the prompt ${prompt}. Format result as HTML.`,
+//       temperature: 0.7,
+//       max_tokens: 256,
+//       top_p: 1,
+//       frequency_penalty: 0,
+//       presence_penalty: 0,
+//     });
+
+//     const qsresult = response.data.choices[0].text;
+//     res.json(qsresult);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('An error occurred while processing your request.');
+//   }
+// });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
