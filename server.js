86ee22047c126ec2bb19ef62d7bec751281cf36b
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-05-12 23:34:23 +0530

    new server

--- server.js
+++ server.js
@@ -1,43 +1,18 @@
 require("dotenv").config();
-const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require("openai");
-const path = require('path');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const allowedOrigins = ['https://gyanl.com/wordpad/', 'https://gyanl.github.io', 'http://gyanl.com', 'http://gyanl.github.io' ];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  optionsSuccessStatus: 200 // For legacy browser support
-}
-
-app.use(cors(corsOptions));
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://gyanl.com');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 app.get('/quickstart/:prompt', async (req, res) => {
@@ -45,7 +20,7 @@ app.get('/quickstart/:prompt', async (req, res) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Generate a title and 6 creative lines with the prompt ${prompt}. Format result as HTML.",
+      prompt: `Generate a title and 6 creative lines with the prompt ${prompt}. Format result as HTML.`,
       temperature: 0.7,
       max_tokens: 256,
       top_p: 1,
@@ -65,4 +40,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
